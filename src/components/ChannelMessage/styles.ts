b67857111cf00fdd6c670d41f5a9fa4232bac75e
João Mateus Games Coelho <50842268+mateusgcoelho@users.noreply.github.com>
2020-07-31 00:41:37 -0300

    Update styles.ts

--- src/components/ChannelMessage/styles.ts
+++ src/components/ChannelMessage/styles.ts
@@ -19,6 +19,7 @@ export const Container = styled.div`
 export const Avatar = styled.div`
   width: 40px;
   height: 40px;
+  flex-shrink: 0;
   background-color: var(--secondary);
   border-radius: 50%;
   &.bot {
